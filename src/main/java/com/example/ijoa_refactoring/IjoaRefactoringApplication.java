ac10940a882d73cc818c0feb2e60f0225f8cf062
KimMinHwan <127469367+rlaalsghks8@users.noreply.github.com>
2024-02-22 15:19:35 +0900

    feat:"프로젝트 생성"

---
+++ src/main/java/com/example/ijoa_refactoring/IjoaRefactoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.ijoa_refactoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjoaRefactoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IjoaRefactoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ijoa_refactoring/IjoaRefactoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ijoa_refactoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjoaRefactoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
