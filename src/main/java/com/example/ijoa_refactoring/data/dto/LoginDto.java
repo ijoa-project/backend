bfbaafec2e81e226583b52132669e2cd8b96cc49
KimMinHwan <127469367+rlaalsghks8@users.noreply.github.com>
2024-03-01 17:13:35 +0900

    "BUG":"메인 push test"

--- src/main/java/com/example/ijoa_refactoring/data/dto/LoginDto.java
+++ src/main/java/com/example/ijoa_refactoring/data/dto/LoginDto.java
@@ -2,10 +2,12 @@ package com.example.ijoa_refactoring.data.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @Getter
 @NoArgsConstructor
+@Setter
 public class LoginDto {
     private String username;
     private String password;
