cf9768e8729f3ba7704a8130ed4b3453c3798394
KimMinHwan <127469367+rlaalsghks8@users.noreply.github.com>
2024-03-01 17:25:28 +0900

    "FEAT":"메인 push 누락된거 추가"

--- src/main/java/com/example/ijoa_refactoring/service/UserService.java
+++ src/main/java/com/example/ijoa_refactoring/service/UserService.java
@@ -1,19 +1,40 @@
 package com.example.ijoa_refactoring.service;
 
 import com.example.ijoa_refactoring.data.dto.JoinDto;
+import com.example.ijoa_refactoring.data.dto.LoginDto;
+import com.example.ijoa_refactoring.data.dto.TokenDto;
 import com.example.ijoa_refactoring.data.entity.Dolbomi;
 import com.example.ijoa_refactoring.data.entity.Parent;
+import com.example.ijoa_refactoring.jwt.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.ijoa_refactoring.data.repository.DolbomiRepository;
 import com.example.ijoa_refactoring.data.repository.ParentRepository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class UserService {
     private DolbomiRepository dolbomiRepository;
     private ParentRepository parentRepository;
+    //private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AuthenticationManagerBuilder managerBuilder;
+    private final TokenProvider tokenProvider;
 
-    @Autowired
+
+    public UserService(DolbomiRepository dolbomiRepository,ParentRepository parentRepository, AuthenticationManagerBuilder managerBuilder, TokenProvider tokenProvider) {
+        this.dolbomiRepository = dolbomiRepository;
+        this.parentRepository = parentRepository;
+        this.managerBuilder = managerBuilder;
+        this.tokenProvider = tokenProvider;
+    }
+
+
+
     public void registerUser(JoinDto joinDto){
         if(joinDto.equals("dolbomi")){
             Dolbomi dolbomi = new Dolbomi();
@@ -37,5 +58,15 @@ public class UserService {
             parentRepository.save(parent);
 
         }
+    }
+
+    @Transactional
+    public TokenDto login(LoginDto requestDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken = requestDto.toAuthentication(requestDto.getUsername(),requestDto.getPassword());
+
+        Authentication authentication = managerBuilder.getObject().authenticate(authenticationToken);
+
+        return tokenProvider.generateTokenDto(authentication);
     }
 }
